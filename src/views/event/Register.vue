5068be85bab8808a60feaa5147aaa402e230e29b
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-08-11 16:13:15 +0300

    Added Programatic Navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,10 +1,20 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      // Call to API
+      //  If registered sucessfully, then rdirect to event details
+      this.$router.push({
+        name: 'EventDetails',
+      })
+    },
+  },
 }
 </script>
 
