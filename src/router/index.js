93520bb90f6634c54a97efed1135df0d5b53c29f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-08-11 14:30:45 +0300

    Aded Pagination

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import EventDetails from "@/views/EventDetails.vue"
+import EventList from "../views/EventList.vue"
+import EventDetails from "../views/EventDetails.vue"
+import About from "../views/About.vue"
 
 const routes = [
   {
     path: "/",
     name: "EventList",
     component: EventList,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    // Parse & set the current page from the router using Function mode.
+    // If page exists parse the string to an integer, otherwise return 1.    
+    props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
     path: "/event/:id",
     name: "EventDetails",
     props: true,
     component: EventDetails,
-  }
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
 ];
 
 const router = createRouter({
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,26 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-Link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+      >
+        &#60; Prev Page
+      </router-Link>
+
+      <router-Link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        Next Page &#62;
+      </router-Link>
+    </div>
   </div>
 </template>
 
@@ -9,25 +29,40 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
 
 export default {
   name: 'EventList',
+  props: ['page'],
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      totalEvents: 0,
     }
   },
+
+  // Lifecycle Hook
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      this.events = null
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2)
+
+      return this.page < totalPages
+    },
   },
 }
 </script>
@@ -37,5 +72,20 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination{
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev{
+  text-align:right;
 }
 </style>
